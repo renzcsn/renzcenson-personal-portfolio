37ade59559cbe29e36dc36a2c3f4a75067190676
RzCs1 <rzmedia02@gmail.com>
2026-04-27 18:19:19 +0800

    fix images 2

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,14 +3,14 @@ function Background() {
     <div className="w-screen h-screen absolute top-0 left-0 -z-10 overflow-hidden" style={{ backgroundColor: '#020D19' }}>
       {/* Animated Smoke SVGs */}
       <img
-        src="../assets/images/Smoke.svg"
+        src="/assets/images/Smoke.svg"
         alt="Smoke"
         className="absolute left-[0] top-0 w-[550px] opacity-80 animate-smoke-move pointer-events-none select-none"
         draggable="false"
         aria-hidden="true"
       />
       <img
-        src="../assets/images/Smoke.svg"
+        src="/assets/images/Smoke.svg"
         alt="Smoke 1"
         className="absolute right-[0] bottom-0 w-[700px] opacity-60 animate-smoke-move-reverse pointer-events-none select-none"
         draggable="false"
