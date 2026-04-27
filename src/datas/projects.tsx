e9c23be6bc362c183d79158eb6975601a456c3e6
RzCs1 <rzmedia02@gmail.com>
2026-04-27 18:08:19 +0800

    fix images

--- src/datas/projects.tsx
+++ src/datas/projects.tsx
@@ -1,3 +1,12 @@
+import learnup from "../assets/images/learnup/thumbnail.png";
+import foodhunt from "../assets/images/foodhunt/thumbnail.png";
+import cyberduo from "../assets/images/cyberduo/thumnail.png";
+import readysetcode from "../assets/images/readysetcode/thumbnail.png";
+import teachersday from "../assets/images/pubmats/teachersday.png";
+import allsoulsday from "../assets/images/pubmats/allsoulsday.png";
+import newyear from "../assets/images/pubmats/newyear.png";
+import goodluckmidterm from "../assets/images/pubmats/goodluckmidterm.png";
+
 export type Project = {
 	id: string;
 	title: string;
@@ -17,7 +26,7 @@ export const projects: Project[] = [
 		description:
 			"LearnUp is a platform that offers a wide range of courses and tutorials on various topics, including web development, mobile app development, and more. Whether you're a beginner or an experienced developer, you'll find something that suits your needs.",
 		tags: "UI/UX Design · Figma · Prototype",
-		thumbnailUrl: "src/assets/images/learnup/thumbnail.png",
+		thumbnailUrl: learnup,
 		imagesUrl: [""],
 		colors: ["#F5F5F5", "#FFFFFF"],
 		typefaces: [""],
@@ -31,7 +40,7 @@ export const projects: Project[] = [
 		description:
 			"Food Hunt is a mobile app that helps you find the best restaurants and food spots in your area. With a user-friendly interface and a vast database of eateries, Food Hunt makes it easy to discover new dining experiences and satisfy your cravings.",
 		tags: "UI/UX Design · Figma · Prototype",
-		thumbnailUrl: "src/assets/images/foodhunt/thumbnail.png",
+		thumbnailUrl: foodhunt,
 		imagesUrl: [""],
 		colors: ["#F5F5F5", "#FFFFFF"],
 		typefaces: [""],
@@ -45,7 +54,7 @@ export const projects: Project[] = [
 		description:
 			"Explore the world of mobile games and discover strategies to beat your friends and enemies. Whether you're a casual gamer or a hardcore gamer, we have something for everyone.",
 		tags: "HTML · CSS · JavaScript",
-		thumbnailUrl: "src/assets/images/cyberduo/thumnail.png",
+		thumbnailUrl: cyberduo,
 		imagesUrl: [""],
 		colors: ["#F5F5F5", "#FFFFFF"],
 		typefaces: [""],
@@ -59,7 +68,7 @@ export const projects: Project[] = [
 		description:
 			"Ready Set Code is a platform that offers a wide range of courses and tutorials on various topics, including web development, mobile app development, and more. Whether you're a beginner or an experienced developer, you'll find something that suits your needs.",
 		tags: "HTML · CSS · JavaScript",
-		thumbnailUrl: "src/assets/images/readysetcode/thumbnail.png",
+		thumbnailUrl: readysetcode,
 		imagesUrl: [""],
 		colors: ["#F5F5F5", "#FFFFFF"],
 		typefaces: [""],
@@ -73,7 +82,7 @@ export const projects: Project[] = [
 		description:
 			"In celebration of World Teachers' Day 2025, we created a captivating poster design that pays tribute to the dedication and impact of educators worldwide.",
 		tags: "PUBMAT · Figma · Layout",
-		thumbnailUrl: "src/assets/images/pubmats/teachersday.png",
+		thumbnailUrl: teachersday,
 		imagesUrl: [""],
 		colors: ["#F5F5F5", "#FFFFFF"],
 		typefaces: [""],
@@ -87,7 +96,7 @@ export const projects: Project[] = [
 		description:
 			"In commemoration of All Souls' Day 2025, we designed a poignant poster that honors the memory of departed loved ones and celebrates the enduring bond between the living and the deceased.",
 		tags: "PUBMAT · Figma · Layout",
-		thumbnailUrl: "src/assets/images/pubmats/allsoulsday.png",
+		thumbnailUrl: allsoulsday,
 		imagesUrl: [""],
 		colors: ["#F5F5F5", "#FFFFFF"],
 		typefaces: [""],
@@ -101,7 +110,7 @@ export const projects: Project[] = [
 		description:
 			"In celebration of the new year, we created a vibrant poster that captures the excitement and possibilities of the year ahead.",
 		tags: "PUBMAT · Figma · Layout",
-		thumbnailUrl: "src/assets/images/pubmats/newyear.png",
+		thumbnailUrl: newyear,
 		imagesUrl: [""],
 		colors: ["#F5F5F5", "#FFFFFF"],
 		typefaces: [""],
@@ -115,7 +124,7 @@ export const projects: Project[] = [
 		description:
 			"Wishing all students the best of luck on their midterm exams! May your hard work and dedication pay off with great results.",
 		tags: "PUBMAT · Figma · Layout",
-		thumbnailUrl: "src/assets/images/pubmats/goodluckmidterm.png",
+		thumbnailUrl: goodluckmidterm,
 		imagesUrl: [""],
 		colors: ["#F5F5F5", "#FFFFFF"],
 		typefaces: [""],
